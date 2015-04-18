67e0ecbfaef0c6f92c9568e28a51ffa5810bb33f
kothead <gdmmtf@gmail.com>
2015-04-18 13:15:14 +0500

    initial commit

---
+++ android/src/com/vdroog1/shamans/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.vdroog1.shamans.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vdroog1.shamans.ShamanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ShamanGame(), config);
+	}
+}
---
+++ desktop/src/com/vdroog1/shamans/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vdroog1.shamans.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vdroog1.shamans.ShamanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShamanGame(), config);
+	}
+}
